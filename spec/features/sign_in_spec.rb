8aba3dac9c955bfac8dc0086c7a9c2b33b6ad3db
buiquangthang <buiquangthangit@gmail.com>
2016-09-18 22:52:00 +0700

    passing feature test for devise login

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Sign in', :devise do
   scenario 'user cannot sign in if not registered' do
     signin('person@example.com', 'password')
-    expect(page).to have_content 'Invalid email or password'
+    expect(page).to have_content 'Invalid Email or password'
   end
 
   scenario 'user can sign in with valid credentials' do
@@ -15,12 +15,12 @@ feature 'Sign in', :devise do
   scenario 'user cannot sign in with invalid email' do
     user = FactoryGirl.create(:user)
     signin('invalid@email.com', user.password)
-    expect(page).to have_content 'Invalid email or password'
+    expect(page).to have_content 'Invalid Email or password'
   end
 
   scenario 'user cannot sign in with invalid password' do
     user = FactoryGirl.create(:user)
     signin(user.email, 'invalidpass')
-    expect(page).to have_content 'Invalid email or password'
+    expect(page).to have_content 'Invalid Email or password'
   end
 end
