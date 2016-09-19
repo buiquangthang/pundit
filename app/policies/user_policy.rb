00354f6974ceba323db21c0ccb9fe9ec07ef48ba
buiquangthang <buiquangthangit@gmail.com>
2016-09-19 11:09:10 +0700

    Update and destroy are working

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -6,4 +6,20 @@ class UserPolicy
     @user = model
   end
 
+  def index?
+    @current_user.admin?
+  end
+
+  def show?
+    @current_user.admin? || @current_user == @user
+  end
+
+  def update?
+    @current_user.admin?
+  end
+
+  def destroy?
+    return false if @current_user == @user
+    @current_user.admin?
+  end
 end
